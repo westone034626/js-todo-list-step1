3941830c7a66f57630450e784a133a4136acb954
westone034626 <dydrlaks@likelion.org>
2020-12-01 15:17:59 +0900

    feat: Add function: show todoItems by status

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const input = document.querySelector("input");
 const todoList = document.querySelector(".todo-list");
 const todo_count_container = document.querySelector(".todo-count");
 const todo_count = todo_count_container.querySelector("strong");
+const filters = document.querySelector(".filters");
+
+let allTodos = [];
+
+function makeEmpty() {
+  console.log("executed makeEmpty");
+  while (todoList.firstChild) {
+    todoList.removeChild(todoList.firstChild);
+  }
+}
 
 function updateToDoCount() {
   todo_count.innerHTML = todoList.children.length;
@@ -36,6 +46,10 @@ function handleDelete(event) {
   if (isDelBtn) {
     const li = isDelBtn.parentNode.parentNode;
     todoList.removeChild(li);
+    const newAllTodos = allTodos.filter(function (todo) {
+      return parseInt(li.id) !== todo.id;
+    });
+    allTodos = newAllTodos;
     updateToDoCount();
   }
 }
@@ -44,31 +58,89 @@ function handleComplete(event) {
   const isCheckBox = event.target.closest(".toggle");
   if (isCheckBox && isCheckBox.checked) {
     event.target.closest("li").classList.add("completed");
+    for (let i of allTodos) {
+      if (i.id === parseInt(event.target.closest("li").id)) {
+        i.isActive = false;
+      }
+    }
   } else {
     event.target.closest("li").classList.remove("completed");
+    for (let i of allTodos) {
+      if (i.id === parseInt(event.target.closest("li").id)) {
+        i.isActive = true;
+      }
+    }
   }
+  console.log("after complete process: ", allTodos);
 }
 
-function createToDoItem(text) {
+function createToDoItem(todo) {
   const li = document.createElement("li");
+  li.id = todo.id;
+  if (todo.isActive === false) {
+    li.classList.add("completed");
+    isChecked = "checked";
+  } else {
+    isChecked = "";
+  }
   li.innerHTML = `
     <div class="view">
-      <input class="toggle" type="checkbox" />
-      <label class="label">${text}</label>
+      <input class="toggle" type="checkbox" ${isChecked}/>
+      <label class="label">${todo.text}</label>
       <button class="destroy"></button>
     </div>
     <input class="edit" value="새로운 타이틀" />
   `;
   todoList.appendChild(li);
+  const todoObj = { id: todo.id, text: todo.text, isActive: todo.isActive };
+  return todoObj;
 }
 
 function handleSubmit(event) {
-  const targetValue = event.target.value;
-  if (event.key == "Enter" && targetValue !== "") {
-    createToDoItem(targetValue);
+  const todoObj = {
+    text: event.target.value,
+    id: allTodos.length + 1,
+    isActive: true,
+  };
+  if (event.key == "Enter" && event.target.value !== "") {
+    allTodos.push(createToDoItem(todoObj));
     input.value = "";
     updateToDoCount();
   }
+}
+
+function handleFilter(event) {
+  const lis = filters.querySelectorAll("a");
+  const isAll = event.target.closest(".all");
+  const isActive = event.target.closest(".active");
+  const isCompleted = event.target.closest(".completed");
+  let currentTodos = [];
+
+  if (isAll) {
+    isAll.classList.add("selected");
+    lis[1].classList.remove("selected");
+    lis[2].classList.remove("selected");
+    currentTodos = allTodos;
+  } else if (isActive) {
+    isActive.classList.add("selected");
+    lis[0].classList.remove("selected");
+    lis[2].classList.remove("selected");
+    currentTodos = allTodos.filter(function (todo) {
+      return todo.isActive;
+    });
+  } else if (isCompleted) {
+    isCompleted.classList.add("selected");
+    lis[0].classList.remove("selected");
+    lis[1].classList.remove("selected");
+    currentTodos = allTodos.filter(function (todo) {
+      return todo.isActive === false;
+    });
+  }
+
+  makeEmpty();
+  currentTodos.forEach(function (todo) {
+    createToDoItem(todo);
+  });
 }
 
 function init() {
@@ -76,6 +148,7 @@ function init() {
   todoList.addEventListener("click", handleComplete);
   todoList.addEventListener("click", handleDelete);
   todoList.addEventListener("dblclick", handleDBClick);
+  filters.addEventListener("click", handleFilter);
 }
 
 init();
